00253ab4d6da56cc1caaa5717fb7a8d898ce06ea
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2025-09-01 15:08:51 -0400

    Initial Commit

---
+++ Chet/ChetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChetApp.swift
+//  Chet
+//
+//  Created by gian singh on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ChetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChetTests/ChetTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChetTests.swift
+//  ChetTests
+//
+//  Created by gian singh on 9/1/25.
+//
+
+import Testing
+@testable import Chet
+
+struct ChetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
